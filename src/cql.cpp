d8722ad804a88271332b4adcf47663950c6485ba
Dmitry Ponyatov <dponyatov@gmail.com>
2025-10-30 17:04:29 +0400

    .

--- src/cql.cpp
+++ src/cql.cpp
@@ -1,8 +1,16 @@
 #include "cql.hpp"
 
+#include <iostream>
+#include <thread>
+
+void hello() { std::clog << "Hello\n"; }
+
 int main(int argc, char *argv[]) {
     arg(0, argv[0]);
-    
+    //
+    std::thread t(hello);
+    std::this_thread::yield();
+    //
     for (int i = 1; i < argc; i++) {
         arg(i, argv[i]);
         yyfile = argv[i];
@@ -11,10 +19,13 @@ int main(int argc, char *argv[]) {
         fclose(yyin);
         yyfile = nullptr;
     }
+    //
+    t.join();
+    return 0;
 }
 
 void arg(int argc, char *argv) {  //
-    std::cerr << "arg[" << argc << "] = <" << argv << ">\n";
+    std::clog << "arg[" << argc << "] = <" << argv << ">\n";
 }
 
 void yyerror(std::string msg) {
