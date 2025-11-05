c8513a5115b157ffa1dc4f3ab1292716872b087b
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-05 11:55:32 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

---
+++ src/json2cpp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os, sys, re
+import json
+
+if __name__ == "__main__":
+    with open(sys.argv[1], 'r') as jsn:
+        with open('inc/config.hpp','w') as hpp:
+            print('#pragma once', file=hpp)
+            jsn = json.load(jsn)
+            print(f'#define baseCPUIndex {jsn["baseCPUIndex"]}',file=hpp)
+            print("")
+            print(jsn)
