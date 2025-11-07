97b740106eb6a54d2e7e5b406285a43b913622fc
Dmitry Ponyatov <dponyatov@gmail.com>
2025-11-07 17:49:43 +0400

    .

--- mk/rule.mk
+++ mk/rule.mk
@@ -1,3 +1,3 @@
-bin/$(BINFILE): $(C) $(H) $(CP) $(HP) $(MK) $(CM)
-	cmake --fresh --preset linux
-	cmake --build --preset linux -j
+# bin/$(BINFILE): $(C) $(H) $(CP) $(HP) $(MK) $(CM)
+# 	cmake --fresh --preset linux
+# 	cmake --build --preset linux -j
